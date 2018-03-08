8683c88ef004b505e930c45e821a0a422ab98358
sagsoz06 <designer@visualturk.net>
2018-03-08 04:12:02 +0300

    Fixed product view status

--- Http/Controllers/Admin/ProductController.php
+++ Http/Controllers/Admin/ProductController.php
@@ -105,6 +105,7 @@ class ProductController extends AdminBaseController
                     ));
                     return $action_buttons;
                 })
+                ->escapeColumns([])
                 ->make(true);
         }
 
